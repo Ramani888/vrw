99f064a2eeb1fc5b0eddb74a2f3d5d9b6a7ad845
Ramani888 <ramanidivyesh888@gmail.com>
2025-05-22 20:30:41 +0530

    for testing update delivery charge

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -254,8 +254,8 @@ export default function CheckoutPage() {
     }
   }, [user])
 
-  const deliveryCharge = cartData?.totalDeliveryCharge;
-  // const deliveryCharge = 1;
+  // const deliveryCharge = cartData?.totalDeliveryCharge;
+  const deliveryCharge = 1;
 
   const totalPrice = cartData?.data?.reduce((accumulator: any, item: any) => {
     return accumulator + item?.product?.mrp * item?.qty;
